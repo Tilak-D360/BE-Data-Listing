fe4db0b1487d2518bf425c9f68824906c0a66419
Tilak-D360 <123104377+Tilak-D360@users.noreply.github.com>
2023-01-21 00:31:15 +0530

    Update main.py

--- BE-Data-Fetch/main.py
+++ BE-Data-Fetch/main.py
@@ -1,2 +1,9 @@
-def hello_world(request):
-  return 'hello - world!'
+from BE_Index import BE_Fetch_data
+from BE_utils import initialise
+
+def main(request):
+    global total_pages, total_responses, data
+    total_pages, total_responses, data = initialise()
+    update_overall, total_overall = BE_Fetch_data()
+    return "Total Response: " + total_overall + "Updated Response: " + update_overall
+
