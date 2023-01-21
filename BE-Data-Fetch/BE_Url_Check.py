08fd31f5077e97a0a07259ffad67265af42ef9a1
Tilak-D360 <123104377+Tilak-D360@users.noreply.github.com>
2023-01-21 16:44:50 +0530

    URL_procedure changed

--- BE-Data-Fetch/BE_Url_Check.py
+++ BE-Data-Fetch/BE_Url_Check.py
@@ -1,4 +1,4 @@
-# BE Url Checker
+# BE_Url_Checker
 
 # function to check if file at given link exists or not
 import requests
@@ -10,17 +10,17 @@ def BE_check_url_for_pattern(link):
     for pattern in URL_PATTERN :       # check for different pattern in url
         if(pattern in link ):     
             return None, pattern + " found in URL"
-    response = requests.get(link)
-    URL_SET.add(link)
-    if(response.status_code >= 200 and response.status_code <= 299):
-        return response, "Response generated"
-    return None, "Non responsive Link"
+    # response = requests.get(link)
+    # if(response.status_code >= 200 and response.status_code <= 299):
+        # return None, "Response generated"
+    return None, "Link"
 
 def BE_Check_url(link):
-  if(validators.url(link) == True):     # validate url
-      if(link not in URL_SET):         # check for repeatative url
-        return BE_check_url_for_pattern(link)
-      else:  
-        return None, "Repeatative URL"
-
-  return None, "Invalid URL format"
+  if(link not in URL_SET):         # check for repeatative url
+    URL_SET.add(link)
+    if(validators.url(link) == True):     # validate url
+      return BE_check_url_for_pattern(link)
+    else:
+      return None, "Invalid URL format"
+  return None, "Repeatative URL"
+      
